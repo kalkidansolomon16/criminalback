9185c69bacd15f91c60b6b6a675877c5d12c4169
complatform <kalkidansol1623@gmail.com>
2025-03-04 02:16:31 +0300

    create role controller and model

--- routes/api.php
+++ routes/api.php
@@ -4,10 +4,13 @@ use App\Http\Controllers\CaseHistoryController;
 use App\Http\Controllers\CriminalController;
 use App\Http\Controllers\HairTypeController;
 use App\Http\Controllers\RegionController;
+use App\Http\Controllers\RoleController;
 use App\Http\Controllers\TypeController;
 use App\Models\CriminalInformation;
 use App\Models\EducationalLEvel;
+use App\Models\EducationalLevels;
 use App\Models\Region;
+use App\Models\Role;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -20,10 +23,10 @@ Route::get('region/{id}',[RegionController::class,'show']);
 Route::put('region/{id}',[RegionController::class,'update']);
 Route::delete('region/{id}',[RegionController::class,'destroy']);
 //educational Level
-Route::get('education',[EducationalLEvel::class,'index']);
-Route::post('education',[EducationalLEvel::class,'post']);
-Route::put('education/{id}',[EducationalLEvel::class,'update']);
-Route::delete('education/{id}',[EducationalLEvel::class,'destroy']);
+Route::get('education',[EducationalLevels::class,'index']);
+Route::post('education',[EducationalLevels::class,'post']);
+Route::put('education/{id}',[EducationalLevels::class,'update']);
+Route::delete('education/{id}',[EducationalLevels::class,'destroy']);
 //hairType
 Route::get('hair',[HairTypeController::class,'index']);
 Route::post('hair',[HairTypeController::class,'post']);
@@ -49,3 +52,8 @@ Route::get('type',[TypeController::class,'index']);
 Route::post('type',[TypeController::class,'post']);
 Route::put('type/{id}',[TypeController::class,'update']);
 Route::delete('type/{id}',[TypeController::class,'destroy']);
+//role
+Route::get('role',[RoleController::class,'index']);
+Route::post('role',[RoleController::class,'post']);
+Route::put('role/{id}',[RoleController::class,'update']);
+Route::delete('role/{id}',[RoleController::class,'destroy']);
