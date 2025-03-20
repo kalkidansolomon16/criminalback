0592b27ea54e960408031a5f78f020c27d3523fb
complatform <kalkidansol1623@gmail.com>
2025-03-20 14:43:54 +0300

    new commit

--- app/Models/Criminal_information.php
+++ app/Models/Criminal_information.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Criminal_information extends Model
 {
-    //
+    public function criminal(){
+        return $this->belongsTo(Criminal::class);
+    }
+    public function hair_type(){
+        return $this->belongsTo(HairType::class);
+    }
 }
